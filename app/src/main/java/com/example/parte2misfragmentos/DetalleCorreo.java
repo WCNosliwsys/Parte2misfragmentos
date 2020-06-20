0ab77f9fe885c1488c10abee47e47404284d32c6
Wilson Callisaya <demarktec@gmail.com>
2020-06-19 22:28:27 -0500

    Enviando parametros desde el item de un recyclerview a detallecorreo

--- app/src/main/java/com/example/parte2misfragmentos/DetalleCorreo.java
+++ app/src/main/java/com/example/parte2misfragmentos/DetalleCorreo.java
@@ -3,12 +3,18 @@ package com.example.parte2misfragmentos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetalleCorreo extends AppCompatActivity {
 
+    TextView midetalle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_correo);
+        Bundle midato = getIntent().getExtras();
+        midetalle = findViewById(R.id.TxtDetalle);
+        midetalle.setText("De: "+midato.getString("de")+" Asunto: "+midato.getString("asunto")+
+                " contenido: "+midato.getString("texto"));
     }
 }
