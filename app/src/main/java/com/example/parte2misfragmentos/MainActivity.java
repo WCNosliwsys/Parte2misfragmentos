3f903673b19dea8d7ae96805bdaaef6cd19fcf66
Wilson Callisaya <demarktec@gmail.com>
2020-06-19 21:55:18 -0500

    Añadiendo toast segun item del spinner seleccionado

--- app/src/main/java/com/example/parte2misfragmentos/MainActivity.java
+++ app/src/main/java/com/example/parte2misfragmentos/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.parte2misfragmentos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Spinner mispinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mispinner=findViewById(R.id.mispinner);
+        mispinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this,(String)mispinner.getSelectedItem() , Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
